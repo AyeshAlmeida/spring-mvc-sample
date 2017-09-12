4dbc908688f9bcb42f199fd71b26127fd20041d5
AyeshAlmeida <ayesh9206@gmail.com>
2017-09-12 23:36:13 +0530

    New: Localisation Added.

--- src/main/java/com/springmvc/sample/controller/HomeController.java
+++ src/main/java/com/springmvc/sample/controller/HomeController.java
@@ -3,11 +3,23 @@ package com.springmvc.sample.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String getHomePage(){
         return "home";
+    }
+
+    @RequestMapping(value = "/response", method = RequestMethod.GET)
+    @ResponseBody
+    public String getMessageFromApplication(){
+        return "{\"message\":\"Application is up and running.\"}";
+    }
+
+    @RequestMapping
+    public String getLocalisedPage(){
+        return "localised";
     }
 }
---
+++ src/main/webapp/WEB-INF/jsp/localised.jsp
@@ -0,0 +1,28 @@
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
+<html>
+    <head>
+        <title>
+            <spring:message code="localised.title"/>
+        </title>
+    </head>
+
+    <body>
+        <h1>
+            <spring:message code="localised.heading"/>
+        </h1>
+
+        <h2>
+            <spring:message code="localised.sub.heading"/>
+        </h2>
+
+        <p>
+            <spring:message code="localised.paragraph.one"/>
+        </p>
+        <p>
+            <spring:message code="localised.paragraph.two"/>
+        </p>
+        <p>
+            <spring:message code="localised.paragraph.three"/>
+        </p>
+    </body>
+</html>
